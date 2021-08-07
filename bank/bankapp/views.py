dee19ca27f1157c8a9e009db020a50d771231943
nimishyadav29 <70849143+nimishyadav29@users.noreply.github.com>
2021-08-08 00:16:42 +0530

    Update views.py

--- bank/bankapp/views.py
+++ bank/bankapp/views.py
@@ -6,7 +6,7 @@ from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    return render(request ,'index.html')
+    return render(request ,'indexnew.html')
 
 
 def cust(request):
